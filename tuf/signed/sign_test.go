dfd9b4e8ac52ef2f935edd67ebd7ec7b07efe1ab
Riyaz Faizullabhoy <riyaz.faizullabhoy@docker.com>
2016-02-18 09:36:39 -0800

    import refactor
    Signed-off-by: Riyaz Faizullabhoy <riyaz.faizullabhoy@docker.com>

--- tuf/signed/sign_test.go
+++ tuf/signed/sign_test.go
@@ -3,7 +3,6 @@ package signed
 import (
 	"crypto/rand"
 	"encoding/pem"
-	"io"
 	"testing"
 
 	"github.com/docker/notary/cryptoservice"
@@ -65,10 +64,6 @@ func (mts *FailingCryptoService) RemoveKey(keyID string) error {
 	return nil
 }
 
-func (mts *FailingCryptoService) ImportRootKey(r io.Reader) error {
-	return nil
-}
-
 type MockCryptoService struct {
 	testKey data.PrivateKey
 }
@@ -117,10 +112,6 @@ func (mts *MockCryptoService) RemoveKey(keyID string) error {
 	return nil
 }
 
-func (mts *MockCryptoService) ImportRootKey(r io.Reader) error {
-	return nil
-}
-
 var _ CryptoService = &MockCryptoService{}
 
 type StrictMockCryptoService struct {
@@ -158,10 +149,6 @@ func (mts *StrictMockCryptoService) ListAllKeys() map[string]string {
 }
 
 func (mts *StrictMockCryptoService) AddKey(key data.PrivateKey, role string) error {
-	return nil
-}
-
-func (mts *StrictMockCryptoService) ImportRootKey(r io.Reader) error {
 	return nil
 }
 
